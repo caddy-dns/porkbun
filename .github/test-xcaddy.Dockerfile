bc7a7a24cc8072910812722be11218a7a5ff19e3
Niall Fitzpatrick <18366490+Niallfitzy1@users.noreply.github.com>
2025-05-08 21:37:53 +0100

    chore: Add CI workflow and more tests (#26)
    * chore: Add CI workflow and more tests
    
    * chore: improve test cases

---
+++ .github/test-xcaddy.Dockerfile
@@ -0,0 +1,13 @@
+FROM caddy:2-builder-alpine
+
+ARG GITHUB_SHA=main
+ARG GITHUB_REPOSITORY=caddy-dns/porkbun
+
+ENV GITHUB_SHA=$GITHUB_SHA
+ENV GITHUB_REPOSITORY=$GITHUB_REPOSITORY
+
+ADD . .
+
+RUN go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest && \
+    xcaddy build \
+    --with "github.com/caddy-dns/porkbun=."
--- module_test.go
+++ module_test.go
@@ -9,36 +9,91 @@ import (
 )
 
 func TestUnmarshalCaddyFile(t *testing.T) {
-	tests := []string{
-		`porkbun {
+	fmt.Println("Testing valid config parses")
+
+	config := `porkbun {
 			api_key thekey
 			api_secret_key itsasecret
-		}`}
+		}`
 
-	for i, tc := range tests {
-		t.Run(fmt.Sprintf("test case %d", i), func(t *testing.T) {
-			// given
-			dispenser := caddyfile.NewTestDispenser(tc)
-			p := Provider{&porkbun.Provider{}}
-			// when
-			err := p.UnmarshalCaddyfile(dispenser)
-			// then
-			if err != nil {
-				t.Errorf("UnmarshalCaddyfile failed with %v", err)
-				return
-			}
+	// given
+	dispenser := caddyfile.NewTestDispenser(config)
+	p := Provider{&porkbun.Provider{}}
+	// when
+	err := p.UnmarshalCaddyfile(dispenser)
+	// then
+	if err != nil {
+		t.Errorf("UnmarshalCaddyfile failed with %v", err)
+		return
+	}
 
-			expectedAPIKey := "thekey"
-			actualAPIKey := p.Provider.APIKey
-			if expectedAPIKey != actualAPIKey {
-				t.Errorf("Expected APIKey to be '%s' but got '%s'", expectedAPIKey, actualAPIKey)
-			}
+	expectedAPIKey := "thekey"
+	actualAPIKey := p.Provider.APIKey
+	if expectedAPIKey != actualAPIKey {
+		t.Errorf("Expected APIKey to be '%s' but got '%s'", expectedAPIKey, actualAPIKey)
+	}
 
-			expectedAPISecretKey := "itsasecret"
-			actualApiSecretKey := p.Provider.APISecretKey
-			if expectedAPISecretKey != actualApiSecretKey {
-				t.Errorf("Expected ApiSecretKey to be '%s' but got '%s'", expectedAPISecretKey, actualApiSecretKey)
-			}
-		})
+	expectedAPISecretKey := "itsasecret"
+	actualApiSecretKey := p.Provider.APISecretKey
+	if expectedAPISecretKey != actualApiSecretKey {
+		t.Errorf("Expected ApiSecretKey to be '%s' but got '%s'", expectedAPISecretKey, actualApiSecretKey)
+	}
+}
+
+func TestEmptyConfig(t *testing.T) {
+	fmt.Println("Testing empty config fails to parse... ")
+	config := "porkbun"
+
+	dispenser := caddyfile.NewTestDispenser(config)
+	p := Provider{&porkbun.Provider{}}
+
+	err := p.UnmarshalCaddyfile(dispenser)
+	if err == nil {
+		t.Errorf(
+			"UnmarshalCaddyfile should have provided an error, but none was received. api_token = %s, api_secret_key = %s",
+			p.Provider.APIKey,
+			p.Provider.APISecretKey,
+		)
+	}
+}
+
+func TestPartialConfig(t *testing.T) {
+	fmt.Println("Testing partial config fails to parse... ")
+	config := `
+	porkbun {
+		api_secret_key itsasecret
+	}`
+
+	dispenser := caddyfile.NewTestDispenser(config)
+	p := Provider{&porkbun.Provider{}}
+
+	err := p.UnmarshalCaddyfile(dispenser)
+	if err == nil {
+		t.Errorf(
+			"UnmarshalCaddyfile should have provided an error, but none was received. api_token = %s, api_secret_key = %s",
+			p.Provider.APIKey,
+			p.Provider.APISecretKey,
+		)
+	}
+}
+
+func TestTooManyArgs(t *testing.T) {
+	fmt.Println("Testing too many args... ")
+	config := `porkbun {
+			api_key thekey
+			api_secret_key itsasecret
+			something_else fail
+		}`
+
+	dispenser := caddyfile.NewTestDispenser(config)
+	p := Provider{&porkbun.Provider{}}
+
+	err := p.UnmarshalCaddyfile(dispenser)
+	if err == nil {
+		t.Errorf(
+			"UnmarshalCaddyfile should have provided an error, but none was received. api_token = %s, api_secret_key = %s",
+			p.Provider.APIKey,
+			p.Provider.APISecretKey,
+		)
 	}
 }
